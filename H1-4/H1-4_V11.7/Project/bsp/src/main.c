30370edca37fd7b646efe79383270b70bf54c944
guodinglang <guodinglang@sina.com>
2026-07-29 08:52:46 +0800

    上传陶晶驰界面和资料

--- H1-4/H1-4_V11.7/Project/bsp/src/main.c
+++ H1-4/H1-4_V11.7/Project/bsp/src/main.c
@@ -41,45 +41,45 @@ int main(void)
   system_clock_config();
   SysTickConfigure();
   ioConfig();
-  DAC1_Config();
-  swIIC_config();
-  epramInitialize();
-  lcd_init();
+  DAC1_Config(); // TFT_BL_Curr 设置背光电流DAC1
+  swIIC_config(); // EEPROM IIC 1K字节
+  epramInitialize(); // EEPROM初始化 AT24C08C
+  lcd_init(); // TFT液晶初始化
   usart1_config(setups.brUART);
   usart2_config(setups.brDUT); // DUT
   usart3_config(setups.brUSB);
   uart4_config(115200); // DUT2 setups.brDUT
-  SPI2_Config();
-  SPI3_Config();
-  can_config();
-  CH9434_Init();
-  BuzzerIOconfig();
-  set_w5500_netinfo();
+  SPI2_Config(); // SPI2 转网口
+  SPI3_Config(); // SPI3 扩展串口4个
+  can_config(); // CAN 配置
+  CH9434_Init(); // CH9434 初始化
+  BuzzerIOconfig(); //蜂鸣器IO配置
+  set_w5500_netinfo(); //设置W5500以太网模块
   bsp_Init();
   //	configWDT(2500);	//1.6ms*x
 
   //	txCAN1("123456789",9);
 
   {
-    time_wheel_init(&tw);
-    add_timer(&tw, 25, 25, Task_25ms, NULL);
+    time_wheel_init(&tw); // 初始化时间轮
+    add_timer(&tw, 25, 25, Task_25ms, NULL); // 添加25ms定时器
   }
 
   while (1)
   {
-    CH9434Polling();
-    InputIOScan();
-    KeyCallback();
-    ethernet_tcp_server();
-    rxCommandDispose(pUART);
-    rxCommandDispose(pUSB);
-    rxCommandDispose(pEther);
-    DisplayUARTHMI(); // ParameterSetup();
-    SwitchMessage();
-    BuzPoll();
-    WorkStateLED();
+    CH9434Polling(); // CH9434 轮询
+    InputIOScan(); // 按键、开关、旋钮轮询
+    KeyCallback(); // 按键回调函数
+    ethernet_tcp_server(); // TCP服务器
+    rxCommandDispose(pUART); // UART1
+    rxCommandDispose(pUSB); // UART3
+    rxCommandDispose(pEther); // ETH UART4
+    DisplayUARTHMI(); // ParameterSetup(); 通过UART-HMI显示参数设置
+    SwitchMessage(); // 切换消息
+    BuzPoll(); // 蜂鸣器轮询
+    WorkStateLED(); // 工作状态LED
     //		wdt_counter_reload();
 
-    time_wheel_execute(&tw);
+    time_wheel_execute(&tw); // 执行时间轮任务
   }
 }
